bec67988e8bb2237d52ce679e0353b2a237f67ca
Aldmar <aldmarj@users.noreply.github.com>
2024-07-16 13:29:15 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'WEBSITES_ENABLE_APP_CACHE' });
 });
 
 module.exports = router;
